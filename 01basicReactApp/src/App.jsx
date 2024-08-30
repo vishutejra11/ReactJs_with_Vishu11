547656a3b609abb0adae110430cd9927329f69a5
Anil Kumar Patidar <98152428+vishutejra11@users.noreply.github.com>
2024-08-30 16:03:59 +0000

    FirtsReact

---
+++ 01basicReactApp/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+    <h1>Chai & React || Anil Kumar Patidar</h1>
+    <Chai/>
+    </>
+  )      
+}
+
+export default App
---
+++ 01basicReactApp/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai () {
+    return (
+        <h2>Chai In React</h2>
+    );
+}
+
+export default Chai;
